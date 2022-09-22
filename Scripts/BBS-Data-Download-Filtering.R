a57a9a14a8f6192ae286d0982e167e08257de5b8
Rory Macklin <macklin@zoology.ubc.ca>
2022-09-22 08:31:41 -0700

    Updated with Raw_Data and Clean_Data dirs

--- Scripts/BBS-Data-Download-Filtering.R
+++ Scripts/BBS-Data-Download-Filtering.R
@@ -27,38 +27,38 @@ groundhog.library("github::trashbirdecology/bbsAssistant", "2022-09-14")
 
 # Create subdirectory for BBS dataset if one does not already exist.
 
-if(!(dir.exists("./Data/BBS_raw"))) {
-  dir.create("./Data/BBS_raw")
+if(!(dir.exists("./Data/Raw_Data/BBS_raw"))) {
+  dir.create("./Data/Raw_Data/BBS_raw")
 }
 
 # Download 2020 release of the Breeding Bird Dataset using dataset identifier
 # (sb_id) found at https://github.com/TrashBirdEcology/bbsAssistant
 
-grab_bbs_data(sb_id = "5ea04e9a82cefae35a129d65", bbs_dir = "./Data/BBS_raw")
+grab_bbs_data(sb_id = "5ea04e9a82cefae35a129d65", bbs_dir = "./Data/Raw_Data/BBS_raw")
 
 # Delete unnecessary files to save storage space. Retain metadata files for later
 # use.
 
-file.remove("./Data/BBS_raw/50-StopData.zip")
-file.remove("./Data/BBS_raw/MigrantNonBreeder.zip")
+file.remove("./Data/Raw_Data/BBS_raw/50-StopData.zip")
+file.remove("./Data/Raw_Data/BBS_raw/MigrantNonBreeder.zip")
 
 # Decompress ZIP files to access annual total species counts for all routes in
 # British Columbia.
 
-zip::unzip("./Data/BBS_raw/States.zip", exdir = "./Data/BBS_raw/")
-zip:unzip("./Data/BBS_raw/States/BritCol.zip", exdir = "./Data/BBS_raw/British_Columbia/")
+zip::unzip("./Data/Raw_Data/BBS_raw/States.zip", exdir = "./Data/Raw_Data/BBS_raw/")
+zip:unzip("./Data/Raw_Data/BBS_raw/States/BritCol.zip", exdir = "./Data/Raw_Data/BBS_raw/British_Columbia/")
 
 # Remove unused States to save storage space. Retain Washington and Oregon for 
 # use in later analyses
 
-states_delete <- list.files(path = "./Data/BBS_raw/States", full.names = TRUE)
-states_delete <- states_delete[!(states_delete %in% paste0("./Data/BBS_raw/States/", c("BritCol.zip", "Washing.zip", "Oregon.zip")))]
+states_delete <- list.files(path = "./Data/Raw_Data/BBS_raw/States", full.names = TRUE)
+states_delete <- states_delete[!(states_delete %in% paste0("./Data/Raw_Data/BBS_raw/States/", c("BritCol.zip", "Washing.zip", "Oregon.zip")))]
 
 file.remove(states_delete)
 
 # Read and filter British Columbia total annual species counts and filter to
 # Point Grey route (#212).
-PointGrey_TotalCounts <- read_csv("./Data/BBS_raw/British_Columbia/BritCol.csv") %>%
+PointGrey_TotalCounts <- read_csv("./Data/Raw_Data/BBS_raw/British_Columbia/BritCol.csv") %>%
   filter(Route == 212)
 
 # Filter annual total counts for all species at Point Grey route to only include
@@ -73,11 +73,11 @@ PointGrey_BCCH_subset <- PointGrey_BCCH[!(PointGrey_BCCH$Year %in% c(1979, 2000)
 # Write data to cleaned CSV file, if necessary creating a subdirectory for the
 # filtered dataset.
 
-if(!(dir.exists("./Data/BBS_filtered"))) {
-  dir.create("./Data/BBS_filtered")
+if(!(dir.exists("./Data/Clean_Data/BBS_filtered"))) {
+  dir.create("./Data/Clean_Data/BBS_filtered")
 }
 
-write_csv(PointGrey_BCCH, "./Data/BBS_filtered/PointGrey_BCCH.csv")
+write_csv(PointGrey_BCCH, "./Data/Clean_Data/BBS_filtered/PointGrey_BCCH.csv")
 
 
 glm <- glm(data = PointGrey_BCCH_subset, SpeciesTotal ~ Year, family = quasipoisson(link = "log"))
